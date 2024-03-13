1ae02949b74af18f667b7845a9ed8bec7422f6b5
Celtokisa <bluesteelll@hotmail.com>
2024-03-13 15:48:50 +0300

    Basic struct config keyboards handlers

--- run.py
+++ run.py
@@ -1,20 +1,19 @@
 import asyncio
+import logging
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-
-TOKEN = '7184762456:AAGesEvpUcYIRM46M2pGN4KMmaB67Ixmsuc'
+from config import TOKEN
+from app.handlers import router
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
-@dp.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer('Hello world')
 
 async def main():
+    dp.include_router(router)
     await dp.start_polling(bot)
 
+
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     asyncio.run(main())
