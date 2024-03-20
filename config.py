4c7ffd1b9fe5739ff9deade1734554fdb7dc02f6
Celtokisa <bluesteelll@hotmail.com>
2024-03-20 11:34:17 +0300

    changed link to readme

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 
 TOKEN = '7184762456:AAGesEvpUcYIRM46M2pGN4KMmaB67Ixmsuc'
 YOUTUBE_LINK = 'https://www.youtube.com/'
-WIKI_LINK = 'https://github.com/bluesteelll/EpicFightBot/tree/keyboards?tab=readme-ov-file#epicfightbot'
+WIKI_LINK = 'https://github.com/bluesteelll/EpicFightBot/tree/master?tab=readme-ov-file#epicfightbot'
 KILLS_TO_LEVEL_UP = (3, 7, 12, 18, 25, 33, 42, 52)
 GIPHY_URL = 'http://api.giphy.com/v1/gifs/random'
 GIPHY_KEY = '6VfC2ZyKiQ9dekSrqOSSOx3ABiL0vPv7'
